968cd197f685ce231c19c0c7c76584eb58578209
GlenRios <glendariosrodriguez3@gmail.com>
2025-01-19 23:01:00 -0500

    company management

--- backend/app/controllers/user_controller.py
+++ backend/app/controllers/user_controller.py
@@ -1,33 +1,38 @@
-from controllers.__init__ import Iuser, Icompany
 from Configurations.CustomError import CustomError
 
-def login(values: dict):
-    username= values['Username']
-    password= values['Password']
-    return Iuser.find_out(username, password)
-    
-def get(username: str):
-    if username=='SuperAdmin':
-        users= Iuser.get_all()
-        return users
-    user= Iuser.get_by_username(username)
-    name_company= user.Company
-    users= Icompany.get_users(name_company)
-    return Iuser.convert(users)
+class UserController:
 
-def post(values: dict):
-    return Iuser.create(values)
+    def __init__(self, iuser, icompany):
+        self.Iuser= iuser
+        self.Icompany= icompany
+
+    def login(self, values: dict):
+        username= values['Username']
+        password= values['Password']
+        return self.Iuser.find_out(username, password)
+        
+    def get(self, username: str):
+        if username=='SuperAdmin':
+            users= self.Iuser.get_all()
+            return users
+        user= self.Iuser.get_by_username(username)
+        name_company= user.Company
+        users= self.Icompany.get_users(name_company)
+        return self.Iuser.convert(users)
+
+    def post(self, values: dict):
+        return self.Iuser.create(values)
 
 
-def put(values: dict, id: int ):
-    user = Iuser.get(id)
-    if user['Password']== values['Password']:
-        user['Username']= values['Username']
-        if values['NewPassword']:
-            user['Password']= values['NewPassword'] 
-        return Iuser.update(id, user)
-    raise CustomError('Incorrect Password, cant update the user because password is not correct', 500)
+    def put(self, values: dict, id: int ):
+        user = self.Iuser.get(id)
+        if user['Password']== values['Password']:
+            user['Username']= values['Username']
+            if values['NewPassword']:
+                user['Password']= values['NewPassword'] 
+            return self.Iuser.update(id, user)
+        raise CustomError('Incorrect Password, cant update the user because password is not correct', 500)
 
-def delete(id: int):
-    return Iuser.delete(id)
+    def delete(self, id: int):
+        return self.Iuser.delete(id)
 
