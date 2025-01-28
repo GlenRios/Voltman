3a0a987cd3492136bc69306150a914420987fe9c
vextroyer <victormanuel.vena@gmail.com>
2025-01-28 16:48:13 -0500

    For equipment crud make area a selection

--- ui/app/home/branches/page.tsx
+++ ui/app/home/branches/page.tsx
@@ -584,15 +584,19 @@ export default function branchesPage() {
                                 >
                                     Area
                                 </label>
-                                <input
+								{/** Cambios **/}
+                                <select
                                     id="Area"
                                     name="Area"
-                                    type="text"
                                     value={isAddForm ? newEquipment.Area : editFormDataEq.Area}
                                     onChange={handleChange}
                                     className="w-full px-3 py-2 bg-gray-100 dark:bg-gray-700 text-gray-800 dark:text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                                     required
-                                />
+                                >
+									<option value=""> Selecciona un area </option>
+									{areas.map((area) => <option key={area.Name} value={area.Name}>{area.Name}</option>)}
+								</select>
+								{/**  **/}
                             </div>
                             <div>
                                 <label
